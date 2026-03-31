f5e0b56e99494848702bbb911a96a4465fab97c2
Jose Arnedo <arnedo1@msn.com>
2026-03-31 19:04:00 +0200

    changed in cors the frontend url

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: '*',
+        origin: 'https://blog-platform-1-94qc.onrender.com',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
