3a1dfbb3a53c7c534426c91411889052ae6cdba4
Jose Arnedo <arnedo1@msn.com>
2026-03-31 18:08:51 +0200

    ajusted db.js for render deploymend

--- server/db.js
+++ server/db.js
@@ -1,10 +1,8 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user:'josearnedo',
-    host:'localhost',
-    database:'blog_platform',
-    port:5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 })
 
 module.exports = pool
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
 const bcrypt = require('bcrypt');
+require('dotenv').config()
 
 const app = express();
 
