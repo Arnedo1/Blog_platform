3f78e43373bee61529be76932593587ecddbf044
Jose Arnedo <arnedo1@msn.com>
2026-04-01 21:09:16 +0200

    changed the cors so that localhost 5174 also works

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: 'https://blog-platform-1-94qc.onrender.com',
+        origin: ['https://blog-platform-1-94qc.onrender.com', 'http://localhost:5174', 'http://localhost:5173'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
