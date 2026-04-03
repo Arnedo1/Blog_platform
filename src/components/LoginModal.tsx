4aeaca8233441c10e2b64051d18016c36ac149e2
Jose Arnedo <arnedo1@msn.com>
2026-04-03 15:23:15 +0200

    add the option to see the password

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,10 +1,13 @@
 import { useContext, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { LuEye } from 'react-icons/lu';
+import { FiEyeOff } from 'react-icons/fi';
 
 const LoginModal = () => {
     const auth = useContext(AuthContext);
     const [emailLogin, setEmailLogin] = useState<string>('');
     const [passwordLogin, setPasswordLogin] = useState<string>('');
+    const [passwordVisable, setPasswordVisable] = useState<boolean>(false);
     if (!auth) return null;
 
     return (
@@ -21,24 +24,41 @@ const LoginModal = () => {
                         placeholder='Email'
                         onChange={(e) => setEmailLogin(e.target.value)}
                         value={emailLogin}
-                        className='border mb-2 pl-2 w-full rounded-sm border-gray-300 h-10'
+                        className='border focus:outline-none mb-2 pl-2 w-full rounded-sm border-gray-300 h-10'
                         type='text'
                         onKeyDown={(e) =>
                             e.key === 'Enter' &&
                             auth?.login(passwordLogin, emailLogin)
                         }
                     />
-                    <input
-                        placeholder='Paswoord'
-                        onChange={(e) => setPasswordLogin(e.target.value)}
-                        value={passwordLogin}
-                        className='border mb-2 pl-2 w-full rounded-sm border-gray-300 h-10'
-                        type='password'
-                        onKeyDown={(e) =>
-                            e.key === 'Enter' &&
-                            auth?.login(passwordLogin, emailLogin)
-                        }
-                    />
+                    <div className='border mb-2 flex justify-between pl-2 w-full rounded-sm border-gray-300 h-10'>
+                        <input
+                            placeholder='Paswoord'
+                            onChange={(e) => setPasswordLogin(e.target.value)}
+                            value={passwordLogin}
+                            className='focus:outline-none w-full'
+                            type={passwordVisable ? 'text' : 'password'}
+                            onKeyDown={(e) =>
+                                e.key === 'Enter' &&
+                                auth?.login(passwordLogin, emailLogin)
+                            }
+                        />
+                        <div className=' items-center flex mr-4'>
+                            {passwordVisable === false ? <LuEye
+                                className='size-5 text-gray-500 '
+                                onClick={() =>
+                                    setPasswordVisable(!passwordVisable)
+                                }
+                            /> :
+                            <FiEyeOff
+                                className='size-5 text-gray-500 '
+                                onClick={() =>
+                                    setPasswordVisable(!passwordVisable)
+                                }
+                            />}
+                        </div>
+                    </div>
+
                     <button
                         onClick={() => auth?.login(passwordLogin, emailLogin)}
                         className='bg-blue-700 h-10 w-30 mx-auto rounded-sm text-white px-4'>
